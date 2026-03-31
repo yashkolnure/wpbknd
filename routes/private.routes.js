07ccbac157e951b8d8a06c51fb66596874f6a939
unknown <yashkolnure58@gmail.com>
2026-04-01 04:49:30 +0530

    Initial commit

--- routes/private.routes.js
+++ routes/private.routes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { protect } from "../middleware/auth.js";
+import { updateFCMToken } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
@@ -7,5 +8,7 @@ const router = express.Router();
 router.get("/user/me", protect, async (req, res) => {
   res.json(req.user);
 });
+
+router.patch("/auth/fcm-token", protect, updateFCMToken);
 
 export default router;
